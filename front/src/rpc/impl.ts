807b834247a6be012f174c805e43790e74d26ac0
Brandon Fulljames <bfulljames@wizcorp.jp>
2019-06-13 18:48:07 +0900

    Only call callback once

--- front/src/rpc/impl.ts
+++ front/src/rpc/impl.ts
@@ -3,8 +3,6 @@ declare var gowasm: any;
 export const wasmImpl = (method: any, requestData: any, callback: any) => {
   // Avoid Zalgo, always async!
   setImmediate(() => {
-    const result = gowasm[method.name](requestData, callback);
-
-    callback(null, result);
+    gowasm[method.name](requestData, callback);
   });
 };
